2b28698ea6d818b37c5f12b9ee931f555de83396
jmcabreira <jm_cabreira@id.uff.br>
2020-03-02 15:03:44 +0000

    feat: add hist_missing_values function to helpers.py file

--- Customer_Segmentation_Report/helpers.py
+++ Customer_Segmentation_Report/helpers.py
@@ -1,11 +1,13 @@
 import os 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 
 
-
+# <============================================ create_missing_code_dict ====================================================>
 
 def create_missing_code_dict(feat_missing_code_df):
     '''
@@ -24,7 +26,8 @@ def create_missing_code_dict(feat_missing_code_df):
     return missing_dict
 
 
-
+# <============================================ valid_values_dict =========================================================>
+    
 def valid_values_dict(df,missing_dict):
     ''' 
     Iterate over all the df columns and check if value is valid or a missing code.
@@ -57,7 +60,7 @@ def valid_values_dict(df,missing_dict):
 
     return valid_values_dict
 
-
+# <============================================ missing_values_barplt ===================================================> 
 
 def missing_values_barplt(df, column_name, threshold=50):
     
@@ -81,6 +84,49 @@ def missing_values_barplt(df, column_name, threshold=50):
     ax.set_title("Missing values {} " .format(column_name),fontsize= 15)
     ax.set_ylabel('Number of Missing values', fontsize = 13)
 
+    
+    
+    # <============================================hist_missing_values================================================>
+    
+    def hist_missing_values(df,  threshold = None , greater_or_less = None  ):
+        
+        
+        """
+        Creates a histogram of missing values
+        
+        ARG: 
+        
+        df(dataframe) : dataframe containing columns of missing values
+        threshold (integer) : set the threshold in which the dataframe will be parsed
+        greater_or_less (string) : set the relationship between threshold and dataframe (greater or less than)
+       
+        
+        NOTE: This function needs improvement in terms of testing
+        
+        
+        """
+
+    
+    if threshold == None:
+        df = df
+    else:
+        if greater_or_less == 'less':
+             df = df[df['Percent_nan'] < threshold]
+        elif greater_or_less == 'greater':
+            df = df[df['Percent_nan'] > threshold]
+
+    fig = plt.figure(figsize=(20,7))
+    plt.hist(df['Percent_nan'],bins=np.linspace(0,100,21))
+    plt.title('Distribution of Number of Missing Valer per Feature');
+    plt.xticks(np.linspace(0,100,21))
+    plt.xlabel('% Missing Values')
+    plt.ylabel('Number of Features');
+    
+    
+    
+    
+# <============================================ save_csv============================================================> 
+    
 
 
 def save_csv(df, data_dir, filename):
