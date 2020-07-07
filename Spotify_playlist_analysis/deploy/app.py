820775dbb232aeddd044eaeb4b83830f90e97668
jmcabreira <jm_cabreira@id.uff.br>
2020-07-06 21:13:30 -0300

    feat: change ids

--- Spotify_playlist_analysis/deploy/app.py
+++ Spotify_playlist_analysis/deploy/app.py
@@ -13,10 +13,10 @@ import spotipy
 import spotipy.util as util 
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = '7caa94d5897b435abdd6fc9e06bb8f04'
-client_secret = '694e12a56652423194e4f092e7304f2b'
+client_id = 'client_id'
+client_secret = 'client_secret'
 redirect_uri = 'http://localhost:8000/' 
-user_id = '12162753154'
+user_id = 'user_id'
 
 token = util.prompt_for_user_token(user_id,
                                   'playlist-read-collaborative',
