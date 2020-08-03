758e005f985b8286ef4139a8ef4d1e98d15bfd65
jmcabreira <jm_cabreira@id.uff.br>
2020-08-03 02:24:56 -0300

    feat: Add deploy files

--- Spotify_playlist_analysis/deploy/app.py
+++ Spotify_playlist_analysis/deploy/app.py
@@ -1,22 +1,16 @@
-import os.path
 from flask import Flask, render_template,request
-import os
-import json
-import time
-from datetime import datetime
-import pandas as pd
 from ml_utils import *
-import pickle
-import joblib as jb
+
 
 import spotipy
 import spotipy.util as util 
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = 'client_id'
-client_secret = 'client_secret'
-redirect_uri = 'http://localhost:8000/' 
-user_id = 'user_id'
+client_id = '7caa94d5897b435abdd6fc9e06bb8f04'
+client_secret = '694e12a56652423194e4f092e7304f2b'
+redirect_uri = 'http://localhost:8080/' 
+#redirect_uri = 'https://spt-recommender.herokuapp.com/' 
+user_id = 'your_user_id'
 
 token = util.prompt_for_user_token(user_id,
                                   'playlist-read-collaborative',
@@ -26,14 +20,13 @@ token = util.prompt_for_user_token(user_id,
 
 sp = spotipy.Spotify(auth = token)
 
-app = Flask(__name__)
-scaler = pickle.load(open("models/MinMaxScaler.pickle", "rb"))
-lr_model = jb.load('models/lr_modeel.pk.z')
-model_lgbm = jb.load('models/lgbm_model.pk;.z')
+# create an instance of Flask class
+app = Flask(__name__) 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/result', methods=['POST', 'GET'])
 def result():
@@ -43,14 +36,10 @@ def result():
         artist = request.form['artist']
 
         features_data = get_one_track_features(artist, song,sp)
-        pred = make_prediction(features_data)
-        
+        pred = make_prediction(features_data)   
         return render_template("result.html",result = result, pred = pred)
 
-
 if __name__ == "__main__":
-    #port = int(os.environ.get('PORT', 8000))
-    #app.run(host='0.0.0.0', port=port)
     app.run(debug = True)
 
 
--- Spotify_playlist_analysis/deploy/ml_utils.py
+++ Spotify_playlist_analysis/deploy/ml_utils.py
@@ -1,12 +1,21 @@
 import pickle
-import pandas as pd
 import joblib as jb
+
+model_rf = jb.load('models/rf_model.pk.z')
+model_lgbm = jb.load('models/lgbm_model.pk.z')
 
 
 def get_one_track_features(artist, song,sp):
     
-    features_data = {}
+    """ Acessa a API do spotify e acessa os audio features de uma decterminada música
+    ARG: 
+    artist(string): String informando nome do artista a ser pesquisado
+    song:(string): String informando nome da música a ser pesquisada
+    sp(spotipy object): Chace ve acesso a API 
+    RETURNS:
+    features_data(dictionary): Dicionário contendo as audio features"""
     
+    features_data = {}
     search_query = sp.search(artist)
     
     for i in range(len(search_query['tracks']['items'])):
@@ -20,45 +29,83 @@ def get_one_track_features(artist, song,sp):
             
             features_data['danceability'] = features[0]['danceability']
             features_data['energy'] = features[0]['energy']
-            features_data['loudness'] = features[0]['loudness']
+            
+            #loudness = Normalize_value(features[0]['loudness'], loudness = True )
+            #loudness = round(loudness,4)
+            #features_data['loudness'] = loudness
+            features_data['loudness'] = round(Normalize_value(features[0]['loudness'], loudness = True ),2)
+            
+            
             features_data['mode'] = features[0]['mode']
             features_data['speechiness'] = features[0]['speechiness']
             features_data['acousticness'] = features[0]['acousticness']
             features_data['instrumentalness'] = features[0]['instrumentalness']
             features_data['liveness'] = features[0]['liveness']
             features_data['valence'] = features[0]['valence']
-            features_data['tempo'] = features[0]['tempo']
             
+            #tempo = Normalize_value( features[0]['tempo'] )
+            #tempo = round(tempo,4)
+            #features_data['tempo'] = tempo
+            features_data['tempo'] = round(Normalize_value( features[0]['tempo'] ),4)
+                  
             break
-        
+            
     return features_data       
 
-
-def make_prediction(data): 
-
-    scaler = pickle.load(open("models/MinMaxScaler.pickle", "rb"))
-    lr_model = jb.load('models/lr_modeel.pk.z')
-    model_lgbm = jb.load('models/lgbm_model.pk;.z')
-
-    df = pd.DataFrame([data])
+def make_prediction(data):
     
-    scaled_array = scaler.transform(df)
+    """ Realiza a predição com base na combinação dois algoritmos de machine learning (Ensemble model)
+    ARG:
+    data(dictionary): Dicionário contendo as audio features
 
-    if scaled_array is None:
-        return 'Feature is None' 
+    RETURNS: 
+    classe (float): Float que representa a classe predita"""
+
     
-    pred_lr= lr_model.predict(scaled_array)
-    pred_lgbm = model_lgbm.predict(scaled_array)
-    #print(pred_lr)
+    data = ([list(data.values())])
+    #print(data)
+
+    pred_rf= model_rf.predict(data)
+    pred_lgbm = model_lgbm.predict(data)
+    
+    #print(pred_rf)
     #print(pred_lgbm)
 
-    pred =  0.1*pred_lr + 0.9*pred_lgbm
- 
+    pred =  0.4*pred_rf + 0.6*pred_lgbm
+  
     for i, element in enumerate(pred): 
-        if element <= 0.5:
+        if element < 1:
             pred[i] = 0.0
         else:
-            pred[i] = 1.0 
-    result = ['Essa música é recomendada para Jonathan' if pred[0] == 1 else 'Essa música é recomendada para Emily']
+            pred[i] = 1.0
+    
+    #result = ['Música para o Jhon' if pred[0] == 1 else 'Música para a Emy']
+    
     
     return pred[0]
+
+def Normalize_value(feature, loudness = False): 
+    ''' Normaliza tempo e loudness e pega valor absoluto caso parâmetro passado seja loudness
+
+    ARG: 
+
+    tempo(float): Feature que será normalizada
+    loudness(float): Feature que será normalizada e calculado valor absoluto
+ 
+    RETURNS: 
+    --
+    '''
+    Max_tempo = 204.225
+    Max_loudness = 44.761
+    Min_tempo = 52.24
+    Min_loudness = 0.04
+    
+    if loudness:
+        feature = (feature - Min_loudness) / (Max_loudness - Min_loudness)     
+        return  abs(feature)
+    
+    else:        
+        feature = (feature - Min_tempo) / (Max_tempo - Min_tempo)
+    
+        return feature
+    
